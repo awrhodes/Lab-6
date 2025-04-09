e880a96ce4f6a91cc42005cba7bf60971319f566
awrhodes <awrhodes9723@gmail.com>
2025-04-09 03:55:25 -0400

    steps 3+4

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 class Main {
   public static void main(String[] args) {
     // Use the instructions in Blackboard or instructions.md to complete Lab 6
-    
+    MyMap<String, Integer> creditHours = new MyHashMap<>();
+    creditHours.put("IT-1025", 3);
+    creditHours.put("IT-1050", 3);
+    creditHours.put("IT-1150", 3);
+    creditHours.put("IT-2310", 3);
+    creditHours.put("IT-2320", 4);
+    creditHours.put("IT-2351", 4);
+    creditHours.put("IT-2650", 4);
+    creditHours.put("IT-2660", 4);
+    creditHours.put("IT-2030", 4);
   }
 }
