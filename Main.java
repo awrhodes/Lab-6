1731acdb97d7033b991ea3caca70fd3a23fb5810
awrhodes <awrhodes9723@gmail.com>
2025-04-09 04:01:10 -0400

    final fixes

--- Main.java
+++ Main.java
@@ -12,12 +12,14 @@ class Main {
     creditHours.put("IT-2660", 4);
     creditHours.put("IT-2030", 4);
 
-    System.out.print("IT-1025 in map: " + creditHours.containsKey("IT-1025"));
-    System.out.print("IT-2110 in map: " + creditHours.containsKey("IT-2110"));
+    System.out.println("IT-1025 in map: " + creditHours.containsKey("IT-1025"));
+    System.out.println("IT-2110 in map: " + creditHours.containsKey("IT-2110"));
 
     System.out.println("Entries in map creditHours: " + creditHours);
 
     creditHours.remove("IT-2030");
     creditHours.remove("IT-1150");
+
+    System.out.println("Entries in map creditHours: " + creditHours);
   }
 }
